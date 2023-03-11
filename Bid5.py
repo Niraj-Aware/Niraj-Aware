4cdaa3f9ef9e5db2a521a8e8ca6c9a76afe65d81
Niraj-Aware <92052184+Niraj-Aware@users.noreply.github.com>
2023-03-12 02:38:38 +0530

    Update Bid5.py

--- Bid5.py
+++ Bid5.py
@@ -90,46 +90,48 @@ def main():
         else:
             st.sidebar.error("Username already exists")
 
+   # main program
+def main():
+    st.set_page_config(page_title="Colour Bid Game", page_icon=":money_with_wings:")
+    st.title("Colour Bid Game")
+    
+    # set up session state
+    if 'logged_in' not in st.session_state:
+        st.session_state['logged_in'] = False
+    if 'increment' not in st.session_state:
+        st.session_state['increment'] = 10
+    if 'bids' not in st.session_state:
+        st.session_state['bids'] = {'Red': 0, 'Green': 0, 'Blue': 0}
+    
+    # login or register
+    if st.sidebar.button("Login"):
+        username = st.sidebar.text_input("Username")
+        password = st.sidebar.text_input("Password", type="password")
+        if login(username, password):
+            st.sidebar.success("Login successful!")
+            st.session_state['logged_in'] = True
+            st.session_state['username'] = username
+        else:
+            st.sidebar.error("Invalid username or password")
+    elif st.sidebar.button("Register"):
+        username = st.sidebar.text_input("Username")
+        phone = st.sidebar.text_input("Phone number")
+        email = st.sidebar.text_input("Email")
+        wallet = st.sidebar.number_input("Wallet amount", min_value=0, step=1)
+        password = st.sidebar.text_input("Password", type="password")
+        if register(username, phone, email, wallet, password):
+            st.sidebar.success("Registration successful!")
+            st.session_state['logged_in'] = True
+            st.session_state['username'] = username
+        else:
+            st.sidebar.error("Username already exists")
+    
     # display color cards
     st.write("Click on a color card to place a bid")
     col1, col2, col3 = st.columns(3)
     with col1:
         if st.button("Red"):
-            if st.session_state['logged_in']:
-                username = st.session_state['username']
-                wallet = USERS[username]['wallet']
-                bid_amount = st.number_input("Enter bid amount", min_value=st.session_state['increment'], max_value=wallet, step=st.session_state['increment'])
-            if validate_bid(username, bid_amount):
-                st.session_state['bids']['Red'] += bid_amount
-            st.success("Bid placed successfully!")
-            else:
-                st.error("Insufficient funds")
-            with col2:
-                if
-                    st.button("Green"):
-            if st.session_state['logged_in']:
-                username = st.session_state['username']
-            wallet = USERS[username]['wallet']
-            bid_amount = st.number_input("Enter bid amount", min_value=st.session_state['increment'], max_value=wallet,
-                                         step=st.session_state['increment'])
-            if validate_bid(username, bid_amount):
-                st.session_state['bids']['Green'] += bid_amount
-            st.success("Bid placed successfully!")
-            else:
-            st.error("Insufficient funds")
-            with col3:
-                if
-            st.button("Blue"):
-            if st.session_state['logged_in']:
-                username = st.session_state['username']
-            wallet = USERS[username]['wallet']
-            bid_amount = st.number_input("Enter bid amount", min_value=st.session_state['increment'], max_value=wallet,
-                                         step=st.session_state['increment'])
-            if validate_bid(username, bid_amount):
-                st.session_state['bids']['Blue'] += bid_amount
-            st.success("Bid placed successfully!")
-            else:
-            st.error("Insufficient funds")
+            if st.session_state['logged_in
 
             # display wallet balance
             if st.session_state['logged_in']:
